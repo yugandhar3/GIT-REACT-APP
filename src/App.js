668d9f3dbbd1c9a813555de306854588a9f7edbf
yugandha3 <dyugandhar143.dy@gmail.com>
2022-05-15 17:37:58 +0530

    frist git commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>well come to my frist git react project</h1>
+      <h1>Thank you</h1>
+    </div>
+  );
+}
+
+export default App;
